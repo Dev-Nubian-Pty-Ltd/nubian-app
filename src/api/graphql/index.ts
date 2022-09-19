0f9ec3d05029f742ee59e5d37b52375910209b52
Ziggleton <ziggleton@icloud.com>
2022-09-19 20:39:34 +0200

    add graphql configuration

---
+++ src/api/graphql/index.ts
@@ -0,0 +1,5 @@
+export { default as AuthService } from '@api/graphql/services/auth.service';
+export { default as ChannelsService } from '@api/graphql/services/channels.service';
+export { default as MessagesService } from '@api/graphql/services/messages.service';
+export { default as SignatureService } from '@api/graphql/services/signature.service';
+export { default as UserService } from '@api/graphql/services/user.service';
