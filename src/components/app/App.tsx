c8b48bd4b1fd07638553fe2e6ebe29e9b2852247
Ziggleton <ziggleton@icloud.com>
2022-09-19 15:22:44 +0200

    Add authenticated hook with interval

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from 'react';
+import { SessionManager } from '../session-manager/SessionManager.component';
 import './App.scss';
 
-function App() {
-  return <div className="App">Hello</div>;
-}
+const App: React.FC = () => {
+  const [authenticated, setAuthenticated] = useState<boolean>(true);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setAuthenticated((prev) => !prev);
+    }, 1000 * 60 * 60);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  return <SessionManager authenticated={authenticated} />;
+};
 
 export default App;
