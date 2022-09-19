e550fbfce5d9775384d6b99f18d610c33b4d1269
Ziggleton <ziggleton@icloud.com>
2022-09-19 19:42:41 +0200

    Extend module alias

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
 	resolve: {
 		alias: [
 			{ find: '@root', replacement: path.resolve(__dirname, 'src') },
+			{ find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
 			{ find: '@components', replacement: path.resolve(__dirname, 'src/components') },
 			{ find: '@utils', replacement: path.resolve(__dirname, 'src/utils') },
 		],
