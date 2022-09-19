adff018e10370f72d51d606f1bba1faec4ad4560
Ziggleton <ziggleton@icloud.com>
2022-09-19 19:46:19 +0200

    Add routes to application layout

--- src/components/layouts/application/Application.layout.tsx
+++ src/components/layouts/application/Application.layout.tsx
@@ -1,6 +1,12 @@
 import { AppHeader } from '@components/app-header/Appheader.component';
 import { SideNav } from '@components/side-nav/SideNav.compoenent';
+import { CrmPage } from '@root/pages/crm/Crm.page';
+import { DashboardPage } from '@root/pages/dashboard/Dashboard.page';
+import { NotFound } from '@root/pages/not-found/NotFound.page';
+import { ProjectPage } from '@root/pages/projects/Projects.page';
 import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
 import styles from './Application.module.scss';
 const ApplicationLayout: React.FC = () => {
   const [showMenu, setShowMenu] = useState<boolean>(false);
@@ -14,6 +20,14 @@ const ApplicationLayout: React.FC = () => {
       <AppHeader toggleMenu={handleToggleMenu} />
       <main>
         <SideNav showMenu={showMenu} />
+        <div className={styles['application-content']}>
+          <Routes>
+            <Route path="/*" element={<NotFound />}></Route>
+            <Route index element={<DashboardPage />}></Route>
+            <Route path="/projects" element={<ProjectPage />}></Route>
+            <Route path="/crm" element={<CrmPage />}></Route>
+          </Routes>
+        </div>
       </main>
     </div>
   );
