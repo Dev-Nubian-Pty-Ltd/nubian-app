83934124f3af56aea0a04b51fa21eaafed7b6932
Ziggleton <ziggleton@icloud.com>
2022-09-21 21:40:46 +0200

    add routes to base layout

--- src/components/layouts/base/Base.layout.tsx
+++ src/components/layouts/base/Base.layout.tsx
@@ -1,7 +1,17 @@
+import { NotFound } from '@root/pages/not-found/NotFound.page';
+import { SessionsPage } from '@root/pages/sessions/Sessions.page';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import styles from './Base.module.scss';
 const BaseLayout: React.FC = () => {
-  return <div className={styles['base-layout']}>BaseLayout</div>;
+  return (
+    <div className={styles['base-layout']}>
+      <Routes>
+        <Route path="/*" element={<NotFound />}></Route>
+        <Route index element={<SessionsPage />}></Route>
+      </Routes>
+    </div>
+  );
 };
 
 export { BaseLayout };
