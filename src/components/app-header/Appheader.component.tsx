cf42787182a310a6680d60abd2ca310f2eba0419
Ziggleton <ziggleton@icloud.com>
2022-09-21 21:42:40 +0200

    Remove marker colors, extended order

--- src/components/app-header/Appheader.component.tsx
+++ src/components/app-header/Appheader.component.tsx
@@ -1,7 +1,7 @@
 import { ButtonLink } from '@components/shared/button-link/ButtonLink.component';
 import { Title } from '@components/shared/title/Title.component';
-import { Bars3BottomLeftIcon, ChevronDoubleLeftIcon } from '@heroicons/react/24/outline';
-import React from 'react';
+import { Bars3BottomLeftIcon, ChevronDoubleLeftIcon } from '@heroicons/react/24/solid';
+import React, { useEffect, useState } from 'react';
 import styles from './AppHeader.module.scss';
 
 interface AppheaderProps {
@@ -9,12 +9,37 @@ interface AppheaderProps {
   showingMenu: boolean;
 }
 const AppHeader: React.FC<AppheaderProps> = ({ toggleMenu, showingMenu }) => {
+  const [order, setOrder] = useState<{ [key: string]: string } | null>({
+    first: '1',
+    second: '2',
+  });
+
+  useEffect(() => {
+    const newOrder = showingMenu
+      ? {
+          first: '1',
+          second: '2',
+        }
+      : {
+          first: '2',
+          second: '1',
+        };
+    setOrder(newOrder);
+
+    return () => {
+      setOrder(null);
+    };
+  }, [showingMenu]);
+
   return (
     <div className={styles['application-header']}>
       <div className={styles['header-brand']}>
-        {showingMenu && <ButtonLink callback={toggleMenu} icon={<Bars3BottomLeftIcon className="icon" />} />}
-        <Title text="Nubian" />
-        {!showingMenu && <ButtonLink callback={toggleMenu} icon={<ChevronDoubleLeftIcon className="icon" />} />}
+        <ButtonLink
+          style={{ order: order?.first }}
+          callback={toggleMenu}
+          icon={showingMenu ? <Bars3BottomLeftIcon className="icon" /> : <ChevronDoubleLeftIcon className="icon" />}
+        />
+        <Title style={{ order: order?.second }} text="Nubian" />
       </div>
       <div className={styles['header-middle']}>middle</div>
       <div className={styles['header-user-section']}>user section</div>
