bf79af4bc334ae27ba48a80d6ee0106554300a36
Ziggleton <ziggleton@icloud.com>
2022-09-21 21:39:35 +0200

    extend functionality to include style

--- src/components/shared/button-link/ButtonLink.component.tsx
+++ src/components/shared/button-link/ButtonLink.component.tsx
@@ -5,11 +5,12 @@ interface ButtonLink {
   callback?: any;
   linkText?: string;
   icon?: JSX.Element;
+  style?: any;
 }
-const ButtonLink: React.FC<ButtonLink> = ({ callback, linkText, icon }) => {
+const ButtonLink: React.FC<ButtonLink> = ({ callback, linkText, icon, style }) => {
   const buttonType = linkText ? '' : 'btn-icon';
   return (
-    <a className={`${styles['button-link']} ${styles[buttonType]}`} onClick={callback}>
+    <a className={`${styles['button-link']} ${styles[buttonType]}`} onClick={callback} style={style}>
       {icon && icon}
       {linkText && <span className={styles['link-text']}>{linkText}</span>}
     </a>
--- src/components/shared/title/Title.component.tsx
+++ src/components/shared/title/Title.component.tsx
@@ -4,10 +4,11 @@ import classes from './Title.module.scss';
 interface TitleProps {
   text: string | null | undefined;
   caption?: string | any;
+  style?: any;
 }
-const Title: React.FC<TitleProps> = ({ text, caption }) => {
+const Title: React.FC<TitleProps> = ({ text, caption, style }) => {
   return (
-    <h3 className={classes['title']}>
+    <h3 className={classes['title']} style={style}>
       {text}
       {caption && <small className={classes['caption']}>{caption}</small>}
     </h3>
