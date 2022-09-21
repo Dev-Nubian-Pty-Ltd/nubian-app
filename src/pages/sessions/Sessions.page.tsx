6c2bdef86f563bd6e55d07fe49a99d11b89685e8
Ziggleton <ziggleton@icloud.com>
2022-09-21 21:40:17 +0200

    add sessions page

---
+++ src/pages/sessions/Sessions.page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './Sessions.module.scss';
+const SessionsPage: React.FC = () => {
+  return <div className={styles['base-layout']}>SessionsPage</div>;
+};
+
+export { SessionsPage };
