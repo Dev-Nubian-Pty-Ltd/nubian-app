3b30367d014daf65852f79ec7242bf5c1f51be07
Ziggleton <ziggleton@icloud.com>
2022-09-22 19:19:38 +0200

    Add Landing page

---
+++ src/pages/landing/Landing.page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './Landing.page.module.scss';
+const LandingPage: React.FC = () => {
+  return <div className={styles['landing-page']}>LandingPage</div>;
+};
+
+export { LandingPage };
