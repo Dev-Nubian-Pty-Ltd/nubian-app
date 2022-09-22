5d7782229b31df9ad4bbfcef9795814d6aefd79a
Ziggleton <ziggleton@icloud.com>
2022-09-22 19:20:05 +0200

    Extend sessions page

--- src/pages/sessions/Sessions.page.tsx
+++ src/pages/sessions/Sessions.page.tsx
@@ -1,7 +1,23 @@
+import Login from '@root/components/login/Login.component';
+import Register from '@root/components/register/Register.component';
+import { Tabs } from '@root/components/shared/tabs/Tabs.component';
 import React from 'react';
 import styles from './Sessions.module.scss';
 const SessionsPage: React.FC = () => {
-  return <div className={styles['base-layout']}>SessionsPage</div>;
+  const tabHeadings = ['Login', 'Register'];
+  return (
+    <div className={styles['sessions-page']}>
+      <div className={styles['left']}></div>
+      <div className={styles['right']}>
+        <div className={styles['session-wrapper']}>
+          <Tabs tabHeadings={tabHeadings}>
+            <Login />
+            <Register />
+          </Tabs>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export { SessionsPage };
