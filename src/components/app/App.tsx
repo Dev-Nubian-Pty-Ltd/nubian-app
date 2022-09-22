4b836af5941227ad95157f8341be333f63f28ccf
Ziggleton <ziggleton@icloud.com>
2022-09-22 19:22:47 +0200

    add timer to switch authenticated

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,24 @@
 import { SessionManager } from '@components/session-manager/SessionManager.component';
 import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import './App.scss';
 
+interface LastVisited {
+  path: string | any;
+}
 const App: React.FC = () => {
-  const [authenticated, setAuthenticated] = useState<boolean>(true);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [lastVisited, setLastVisted] = useState<LastVisited>();
+  const [authenticated, setAuthenticated] = useState<boolean>(false);
   useEffect(() => {
     const interval = setInterval(() => {
       setAuthenticated((prev) => !prev);
+      setLastVisted(location.state);
     }, 1000 * 60 * 60);
+
+    !authenticated ? navigate('/sessions') : navigate(lastVisited?.path);
+
     return () => {
       clearInterval(interval);
     };
