3bfcdc38b90d5a40d373bc105ef5409f25c6bde5
Ziggleton <ziggleton@icloud.com>
2022-09-23 18:07:42 +0200

    sessions now observe auth change from mobx

--- src/components/session-manager/SessionManager.component.tsx
+++ src/components/session-manager/SessionManager.component.tsx
@@ -1,17 +1,27 @@
 import { ApplicationLayout } from '@components/layouts/application/Application.layout';
 import { BaseLayout } from '@components/layouts/base/Base.layout';
-import React from 'react';
+import { iRepositoryContext, RepositoryContext } from '@root/api/repository/Repository.context';
+import { User } from '@root/api/repository/sessions/SessionsData.store';
+import { observer } from 'mobx-react-lite';
+import React, { useContext, useEffect, useState } from 'react';
 
-interface SessionProps {
-  authenticated: boolean | undefined;
-}
-const SessionManager: React.FC<SessionProps> = ({ authenticated }) => {
+const SessionManagerView: React.FC = () => {
+  const [user, setuser] = useState<User | null>();
+  const { sessionsDataStore } = useContext<iRepositoryContext>(RepositoryContext);
+  const authenticated: boolean = sessionsDataStore.isAuthenticated();
+
+  useEffect(() => {
+    sessionsDataStore.getUser().then((data) => {
+      setuser(data);
+    });
+  }, [authenticated]);
+
   return (
     <div className="session-manager">
-      {authenticated && <ApplicationLayout />}
+      {authenticated && <ApplicationLayout user={user} />}
       {!authenticated && <BaseLayout />}
     </div>
   );
 };
-
+const SessionManager = observer(SessionManagerView);
 export { SessionManager };
