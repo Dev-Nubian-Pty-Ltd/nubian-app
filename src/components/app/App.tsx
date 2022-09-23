d0c0ab6108710e1cf9f9cdc5ed7e8b7f5310e11a
Ziggleton <ziggleton@icloud.com>
2022-09-23 18:05:13 +0200

    code clean up and refactor

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,29 +1,7 @@
 import { SessionManager } from '@components/session-manager/SessionManager.component';
-import { useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
 import './App.scss';
 
-interface LastVisited {
-  path: string | any;
-}
 const App: React.FC = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const [lastVisited, setLastVisted] = useState<LastVisited>();
-  const [authenticated, setAuthenticated] = useState<boolean>(false);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setAuthenticated((prev) => !prev);
-      setLastVisted(location.state);
-    }, 1000 * 60 * 60);
-
-    !authenticated ? navigate('/sessions') : navigate(lastVisited?.path);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-  return <SessionManager authenticated={authenticated} />;
+  return <SessionManager />;
 };
-
 export { App };
