c903d1860b8b69eefde2f1493a8faad43e47670b
Ziggleton <ziggleton@icloud.com>
2022-09-29 21:49:43 +0200

    Make properties panel oberver view

--- src/components/properties-panel/PropertiesPanel.component.tsx
+++ src/components/properties-panel/PropertiesPanel.component.tsx
@@ -1,17 +1,20 @@
 import { XMarkIcon } from '@heroicons/react/24/outline';
-import React, { useState } from 'react';
+import { RepositoryContext } from '@root/api/repository/Repository.context';
+import { observer } from 'mobx-react-lite';
+import React, { useContext } from 'react';
 import { ButtonLink } from '../shared/button-link/ButtonLink.component';
 import { Title } from '../shared/title/Title.component';
 import styles from './PropertiesPanel.module.scss';
-const PropertiesPanel: React.FC = () => {
-  const [panelClass, setPanelClass] = useState<string>('open');
+const PropertiesPanelView: React.FC = () => {
+  const { propertiesViewStore } = useContext(RepositoryContext);
+  const canShow = propertiesViewStore.canShowPanel();
 
   const handleHidePanel = () => {
-    setPanelClass('close');
+    propertiesViewStore.setShowPanel(false);
   };
 
   return (
-    <div className={`${styles['properties-panel']} ${styles[panelClass]}`}>
+    <div className={`${styles['properties-panel']} ${styles[canShow == undefined ? '' : canShow ? 'open' : 'close']}`}>
       <div className={styles['panel-header']}>
         <Title text="Properties Panel" />
         <ButtonLink icon={<XMarkIcon className="icon" />} callback={handleHidePanel} />
@@ -22,4 +25,5 @@ const PropertiesPanel: React.FC = () => {
   );
 };
 
+const PropertiesPanel = observer(PropertiesPanelView);
 export { PropertiesPanel };
