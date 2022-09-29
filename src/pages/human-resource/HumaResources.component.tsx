446d407aa900d2dfc9b2bec1b193e29a71e375fa
Ziggleton <ziggleton@icloud.com>
2022-09-29 20:07:51 +0200

    Add hr skeleton page

---
+++ src/pages/human-resource/HumaResources.component.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './HumanResources.module.scss';
+const HumanResourcePage: React.FC = () => {
+  return <div className={styles['base-layout']}>HumanResourcePage</div>;
+};
+
+export { HumanResourcePage };
